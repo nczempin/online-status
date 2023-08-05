84705e19e94d1484b0cea26b283238dba30b8780
Nicolai Czempin <nicolai.czempin@gmail.com>
2023-08-05 16:05:17 +0200

    Generate Skeleton with Spring Initializr

---
+++ java/online-status/src/main/java/de/czempin/aesdc/onlinestatus/OnlineStatusApplication.java
@@ -0,0 +1,13 @@
+package de.czempin.aesdc.onlinestatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineStatusApplication.class, args);
+	}
+
+}
---
+++ java/online-status/src/test/java/de/czempin/aesdc/onlinestatus/OnlineStatusApplicationTests.java
@@ -0,0 +1,13 @@
+package de.czempin.aesdc.onlinestatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
